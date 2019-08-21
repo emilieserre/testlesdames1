762c5f1c6e25f65f20a4cd47ba08b0830054eaea
unknown <emilieserre@hotmail.com>
2019-08-21 14:24:17 +0200

    first commit

---
+++ src/constants/Constants.js
@@ -0,0 +1,10 @@
+const Constantes = {
+    URL: {
+        wc: 'https://woocommerce-store.on-its-way.com/wp-json/wc/v2/'
+    },
+    Clés: {
+        ConsumerKey: "ck_4f595d15be125e6c26444c84057fbdbd794ed5d9",
+        ConsumerSecret: 'cs_0a08082d29b8104d5c641143cfbd563377eb2b86'
+    }
+}
+les constantes par défaut d'exportation;
